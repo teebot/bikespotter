15f201419e0a985ed2ef835e2e67391c2db80d3d
teebot <thibaut@cozypixel.com>
2014-05-08 15:23:24 +0200

    retrieving geolocation working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('bikespotterApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, geolocation) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    geolocation.getLocation().then(function(data){
+        $scope.coords = {lat:data.coords.latitude, long:data.coords.longitude};
+    });
   });
