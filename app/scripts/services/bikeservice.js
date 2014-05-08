cad2656ecec2aa41f06cd765ef50f47a81dbe40e
teebot <thibaut@cozypixel.com>
2014-05-08 16:26:04 +0200

    quick and dirty json list

--- app/scripts/services/bikeservice.js
+++ app/scripts/services/bikeservice.js
@@ -2,12 +2,24 @@
 
 angular.module('bikespotterApp')
   .service('Bikeservice', function Bikeservice(geolocation, $http) {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-        
+
+        var stationsList = [];
+
         geolocation.getLocation().then(function(data){
             var coords = {lat:data.coords.latitude, long:data.coords.longitude};
             console.log(coords);
+
+            $http.get('scripts/bikeshare.json').then(function (response) {
+
+                var stations = response.data.stationBeanList;
+                console.log(stations);
+
+                for (var i = 0; i < stations.length; i++)
+                {
+                    stationsList.push(stations[i].stationName);
+                }
+            });
         });
 
-
+        this.StationList = stationsList;
   });
