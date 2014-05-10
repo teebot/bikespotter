0394a5c6bc73ecc01e259ea90f0aa26704df8fb0
teebot <thibautnguyen@gmail.com>
2014-05-10 21:50:45 +0200

    switched origin url from ssh to https

--- Gruntfile.js
+++ Gruntfile.js
@@ -382,7 +382,7 @@ module.exports = function (grunt) {
       },
       pages: {
         options: {
-          remote: 'git@github.com:teebot/teebot.github.io.git',
+          remote: 'https://github.com/teebot/teebot.github.io.git',
           branch: 'gh-pages'
         }
       },
