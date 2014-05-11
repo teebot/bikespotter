b6bacbe6a8c8df7c94e0fd12fc08483abc66875d
teebot <thibautnguyen@gmail.com>
2014-05-11 19:03:28 +0200

    removed yeoman default scope values and added anchors to station lists

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,6 @@
 
 angular.module('bikespotterApp')
   .controller('MainCtrl', function ($scope, $location, Stations) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
 
     Stations.all().then(function(response) {
         $scope.stations = response;
