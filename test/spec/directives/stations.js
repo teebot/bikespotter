36d49f7bd2e9b365a5226881e80622299b639105
teebot <thibautnguyen@gmail.com>
2014-05-16 01:26:30 +0200

    basic seo

---
+++ test/spec/directives/stations.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('Directive: stations', function () {
+
+  // load the directive's module
+  beforeEach(module('bikespotterApp'));
+
+  var scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+});
